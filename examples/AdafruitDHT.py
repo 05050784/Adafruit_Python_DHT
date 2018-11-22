328843ce34139d24e635022abbc6b5efaaccf5c0
05050784 <37134163+05050784@users.noreply.github.com>
2018-11-22 15:15:36 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -28,7 +28,7 @@ import Adafruit_DHT
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
-while len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     sensor = sensor_args[sys.argv[1]]
     pin = sys.argv[2]
 else:
@@ -47,7 +47,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
+while humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
     time.sleep(1)
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
